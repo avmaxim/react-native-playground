74d4ec80a700f5e8e708bd28cb980a38b60a705e
avmaxim <avmax19@gmail.com>
2020-02-19 23:52:52 +0300

    Created basic configuration and project for a sample React Native

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello fucking niggas from Andrew!</Text>
+    </View>
+  );
+} 
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
